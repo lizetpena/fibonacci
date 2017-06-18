d16858a5c783f79c3dfbc608dd128e751fc29263
Lizet Pena De Sola <lipenade@microsoft.com>
2017-06-18 15:08:23 -0500

    Refactoring. Displaying the answer goeson its own method.

--- Fibonnaci/Fibonnaci/Program.cs
+++ Fibonnaci/Fibonnaci/Program.cs
@@ -17,46 +17,62 @@ namespace Fibonnaci
 		/// </summary>
 		/// <param name="args"></param>
 		static void Main(string[] args)
-		{
-			Console.WriteLine("Enter one number from the Fibonacci sequence and hit ENTER: ");
-			long input = 0;
-			Int64.TryParse(Console.ReadLine().Trim(), out input);
+        {
+            Console.WriteLine("Enter one number from the Fibonacci sequence and hit ENTER: ");
+            long input = 0;
+            Int64.TryParse(Console.ReadLine().Trim(), out input);
 
-			long start = 0;
-			long next = 1;
-
-
-			//Initialize
-			List<long> fiboSeq = new List<long>();
-			fiboSeq.Add(start);
-			fiboSeq.Add(next);
-			
-
-			//calculate
-
-			while (input > fiboSeq[fiboSeq.Count-1])
-			{
-				
-				long nextFiboNumber = start + next;
-				fiboSeq.Add(nextFiboNumber);
-
-				start = next;
-				next = nextFiboNumber;
-			}
-
-			if (input == fiboSeq[fiboSeq.Count - 1])
-			{
-				Console.WriteLine(fiboSeq[fiboSeq.Count - 2]);
-				Console.WriteLine(fiboSeq[fiboSeq.Count - 3]);
-
-			}
-			else
-			{
-				Console.WriteLine("ERROR");
-			}
+            long start = 0;
+            long next = 1;
 
 
-			Console.ReadLine();
+            //Initialize
+            List<long> fiboSeq = new List<long>();
+            fiboSeq.Add(start);
+            fiboSeq.Add(next);
+
+
+            //calculate
+
+            while (input > fiboSeq[fiboSeq.Count - 1])
+            {
+
+                long nextFiboNumber = start + next;
+                fiboSeq.Add(nextFiboNumber);
+
+                start = next;
+                next = nextFiboNumber;
+            }
+
+            //display answer
+            DisplayAnswer(input, fiboSeq);
+
+            Console.ReadLine();
+        }
+        /// <summary>
+        /// Displays the two previous Fibonacci numbers given an input
+        /// </summary>
+        /// <param name="input">The integer entered on screen</param>
+        /// <param name="fiboSeq">The list holding the generated Fibonacci sequence so far</param>
+        private static void DisplayAnswer(long input, List<long> fiboSeq)
+        {
+            if ((fiboSeq.Count > 1) && (input == fiboSeq[fiboSeq.Count - 1]))
+            {
+                Console.WriteLine("The two preceding FIbonacci numbers are: ");
+                Console.WriteLine(fiboSeq[fiboSeq.Count - 2]);
+                Console.WriteLine(fiboSeq[fiboSeq.Count - 3]);
+
+            }
+            else if (fiboSeq.Count == 1)
+            {
+                Console.WriteLine("The two preceding FIbonacci numbers are: ");
+                Console.WriteLine(fiboSeq[1]);
+                Console.WriteLine(fiboSeq[0]);
+            }
+            else
+            {
+                Console.WriteLine("ERROR");
+            }
         }
     }
 }
